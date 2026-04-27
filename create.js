6fc400c03ee259c81558bd25e43fd6315fb702ba
kpoovakan <217997837+kpoovakan@users.noreply.github.com>
2026-04-26 17:11:04 -0700

    Refactor create.js for improved structure and clarity

--- create.js
+++ create.js
@@ -11,7 +11,7 @@ globalThis.cssTwo = 'body{background-color:var(--background);color:var(--foregro
 globalThis.cssThree = cssOne+cssTwo;
 console.log(cssThree);
 
-globalThis.jsOne = 'function loadButtons(){document.getElementById("output").innerHTML="<button onclick="play()" class="main-button">PLAY NOW</button>"}function play(){document.getElementById("output").innerHTML="<div class="main-picture"><iframe src="https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1&loop=1&modestbranding=1&controls=0&rel=0"\n\ttitle="muahahahahaha" frameborder="0" allow="autoplay; encrypted-media;" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>"}window.onload=loadButtons;';
+globalThis.jsOne = 'window.onload=loadButtons; function loadButtons() {document.getElementById("output").innerHTML=`<button onclick="play()" class="main-button">PLAY NOW</button>`} function play(){document.getElementById("output").innerHTML = `<div class="main-picture"><iframe src="https://www.youtube.com/embed/xvFZjo5PgG0?autoplay=1&loop=1&modestbranding=1&controls=0&rel=0"title="muahahahahaha" frameborder="0" allow="autoplay; encrypted-media;" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>`}';
 globalThis.jsTwo = 'function loadButtons(){document.getElementById("output").innerHTML="<button onclick="play()" class="main-button">PLAY NOW</button>"}function play(){window.location.href="https://www.youtube.com/watch?v=xvFZjo5PgG0"}window.onload=loadButtons;';
 if (buttonResult == "redirect") {
 globalThis.jsThree = jsTwo;
