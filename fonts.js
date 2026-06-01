1dff4bed940c538fe6283ac5390a2018c9819aea
kpoovakan <217997837+kpoovakan@users.noreply.github.com>
2026-06-01 11:05:37 -0700

    edit element id in system()

--- fonts.js
+++ fonts.js
@@ -2,7 +2,7 @@
 window.onload = googly
 
 function system() {
-  document.getElementById("id").innerHTML = `
+  document.getElementById("fontSelector").innerHTML = `
   </br><label for="fontType">font type: </label><select id="fontType" class="input" onchange="fontSelection()">
   <option value="" disabled selected hidden>select font type</option><option value="serif">serif</option><option value="sans">sans-serif</option><option value="monospace">monospace</option><option value="decorative">decorative</option></select>
   <label for="fontName">font: </label><select id="fontName" class="input" onchange="setFonts()"><option value="" disabled selected hidden>select a font type first!</option></select>
