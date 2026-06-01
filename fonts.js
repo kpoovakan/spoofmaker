8871cda2820357c8ae306f747c07add8a3bb2684
kpoovakan <217997837+kpoovakan@users.noreply.github.com>
2026-06-01 13:47:03 -0700

    Add <option> to change between Google and system fonts

--- fonts.js
+++ fonts.js
@@ -2,6 +2,9 @@
 
 function system() {
   document.getElementById("fontSelector").innerHTML = `
+  </br><label for="fontCollection">connect fonts from </label><select id="fontCollection" class="input" onchange="fontCollection()">
+  <option value="fontGoogly">Google Fonts</option><option value="fontSystem" selected>System Fonts</option></select>
+  <p class="input">Google Fonts is recommended because it offers a wide variety of fonts. however, Google Fonts might not work offline. System Fonts are reliable for offline use, but are much more limited.</p>
   </br><label for="fontType">font type: </label><select id="fontType" class="input" onchange="fontSelection()">
   <option value="" disabled selected hidden>select font type</option><option value="serif">serif</option><option value="sans">sans-serif</option><option value="monospace">monospace</option><option value="decorative">decorative</option></select>
   <label for="fontName">font: </label><select id="fontName" class="input" onchange="setFonts()"><option value="" disabled selected hidden>select a font type first!</option></select>
@@ -11,8 +14,11 @@ function system() {
 
 function googly() {
   document.getElementById("fontSelector").innerHTML = `
+  </br><label for="fontCollection">connect fonts from </label><select id="fontCollection" class="input" onchange="fontCollection()">
+  <option value="fontGoogly" selected>Google Fonts</option><option value="fontSystem">System Fonts</option></select>
+  <p class="input">Google Fonts is recommended because it offers a wide variety of fonts. however, Google Fonts might not work offline. System Fonts are reliable for offline use, but are much more limited.</p>
   </br><input id="fontGoogly" class="input" placeholder="enter Google font name" onchange="fontGooglyCheck()"></input>
-  <p class="input">remember that Google fonts are case sensitive: "Oregano" will work, but "oregano" will not work.</p>
+  <p class="input">remember that Google Fonts is case sensitive: "Oregano" will work, but "oregano" will not work.</p>
   </br><div class="preview" id="fontGooglyPreview">font preview will appear here...</div>
   `;
 }
@@ -28,3 +34,13 @@ function fontGooglyCheck() {
   var fontGooglyPreview = '<p style="font-family:'+fontGooglyName+';">Everyday, I see the quick brown fox jump over the lazy dog.</p>';
   document.getElementById("fontGooglyPreview").innerHTML= fontGooglyPreview;
 }
+
+function fontCollection() {
+  var selected = document.getElementById("fontCollection").value;
+  if (selected == "fontGoogly") {
+    googly();
+  else if (selected == "fontSystem") {
+    system();
+  else {
+    console.log('error in selecting the font "Collection"');
+}}
